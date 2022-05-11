a39d704afd16b8bc5e703ab97f19e65cba9f7589
__Rals <winhave@naver.com>
2022-05-11 19:22:58 +0900

    Added Constant File in Home Page.

---
+++ src/pages/Home/constant.ts
@@ -0,0 +1,13 @@
+export const WhatIsQuestionMakerSubPhrase = [
+  "Question Maker is a website where you can create questions.",
+  "Questions can be manufactured in many different types.",
+  "You can study yourself by going over the questions you've manufactured!",
+];
+
+export const HowToUseSubPhrase = [
+  'By clicking the "Make Questions" tab in the header,',
+  "you can start manufacturing various questions.",
+  "",
+  'By clicking the "Saved Questions" tab in the header,',
+  "you can review all the manufactured questions.",
+];
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import Banner from "../../components/Banner";
 import Phrase from "../../components/Phrase";
+import { HowToUseSubPhrase, WhatIsQuestionMakerSubPhrase } from "./constant";
 
 const Home = () => {
   return (
@@ -9,23 +10,13 @@ const Home = () => {
         animation="fade-left"
         duration="1000"
         mainphrase={["What is Question Maker?"]}
-        subphrase={[
-          "Question Maker is a website where you can create questions.",
-          "Questions can be manufactured in many different types.",
-          "You can study yourself by going over the questions you've manufactured!",
-        ]}
+        subphrase={WhatIsQuestionMakerSubPhrase}
       />
       <Phrase
         animation="fade-right"
         duration="1500"
         mainphrase={["How to use?"]}
-        subphrase={[
-          'By clicking the "Make Questions" tab in the header,',
-          "you can start manufacturing various questions.",
-          "",
-          'By clicking the "Saved Questions" tab in the header,',
-          "you can review all the manufactured questions.",
-        ]}
+        subphrase={HowToUseSubPhrase}
       />
     </>
   );
