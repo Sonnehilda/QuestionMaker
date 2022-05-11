25c5c0e6e67107bdd3414af388290c34b43d459f
__Rals <winhave@naver.com>
2022-05-11 23:03:44 +0900

    :: Optimizations

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,32 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+    #root {
+        height: 0;
+    }
+
+    html, body {
+        margin: 0;
+        padding: 0;
+
+        max-width: 100vw;
+        min-height: 100vh;
+
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+            "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+            sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+
+        overflow: overlay;
+        overflow-x: hidden;
+        -ms-user-select: none; 
+        -moz-user-select: -moz-none;
+        -khtml-user-select: none;
+        -webkit-user-select: none;
+        user-select: none;
+    }
+
     ::-webkit-scrollbar {
         width: 1.5vh;
     }
@@ -10,25 +36,6 @@ const GlobalStyle = createGlobalStyle`
     ::-webkit-scrollbar-thumb {
         border-radius: 1.5vh;
         box-shadow: inset 0 0 1.5vh #a1a1a1;
-    }
-
-    #root {height: 0;}
-
-    html {
-        margin: 0;
-        padding: 0;
-
-        max-width: 100vw;
-        min-height: 100vh;
-
-        overflow: overlay;
-        overflow-x: hidden;
-
-        -ms-user-select: none; 
-        -moz-user-select: -moz-none;
-        -khtml-user-select: none;
-        -webkit-user-select: none;
-        user-select: none;
     }
 `;
 
