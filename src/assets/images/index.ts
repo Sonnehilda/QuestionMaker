238c98f347419e4fce3f34911fcfd957a5d5880b
__Rals <winhave@naver.com>
2022-05-12 13:27:38 +0900

    Added Menu Images.

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,1 +1,5 @@
 export { default as banner } from "./banner.png";
+export { default as mc } from "./mc.png";
+export { default as sa } from "./sa.png";
+export { default as tf } from "./tf.png";
+export { default as fc } from "./fc.png";
