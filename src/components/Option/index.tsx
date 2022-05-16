2785cad9768d66d55b67df18fec3617e91cda37c
__Rals <winhave@naver.com>
2022-05-16 19:58:58 +0900

    Added Option Component.

---
+++ src/components/Option/index.tsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+const Background = styled.div`
+  margin-top: 1.5vh;
+
+  width: 76vh;
+  height: 6vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 2vh;
+  text-align: center;
+
+  border-bottom: 0.1vh solid #000;
+
+  :nth-child(2) {
+    margin-top: 0;
+  }
+  :last-child {
+    margin-bottom: 1.5vh;
+  }
+`;
+
+const Phrase = styled.div`
+  width: 73vh;
+
+  font-size: 2vh;
+  word-break: break-all;
+`;
+
+const Delete = styled.div`
+  width: 3vh;
+
+  color: red;
+  font-size: 1.5vh;
+  font-weight: 600;
+
+  cursor: pointer;
+  transition: filter 0.25s;
+
+  :hover {
+    filter: brightness(75%);
+  }
+`;
+
+interface OptionsProps {
+  options: string[];
+  setOptions: React.Dispatch<React.SetStateAction<string[]>>;
+  index: number;
+  option: string;
+}
+
+const Options = ({ options, setOptions, index, option }: OptionsProps) => {
+  const deleteOption = () => {
+    setOptions(
+      options.filter((s) => {
+        return s !== option;
+      })
+    );
+  };
+
+  return (
+    <Background>
+      <Phrase>
+        {index}. {option}
+      </Phrase>
+      <Delete onClick={() => deleteOption()}>✕</Delete>
+    </Background>
+  );
+};
+
+export default Options;
