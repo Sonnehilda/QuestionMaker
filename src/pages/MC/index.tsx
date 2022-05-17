2627fc1c66f832036f330bbdadb7de572a3adefe
__Rals <winhave@naver.com>
2022-05-17 19:09:27 +0900

    Updated Multiple Choice Question Page.

--- src/pages/MC/index.tsx
+++ src/pages/MC/index.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import Banner from "../../components/Banner";
 import McForm from "../../components/McForm";
+import WarningModal from "../../components/WarningModal";
 
 const MakeMultipleChoiceQuestion = () => {
+  const [warning, setWarning] = useState<string>("");
+
   return (
     <>
+      {warning !== "" && (
+        <WarningModal warning={warning} setWarning={setWarning} />
+      )}
       <Banner />
-      <McForm animation="fade-up" duration="1000" />
+      <McForm animation="fade-up" duration="1000" setWarning={setWarning} />
     </>
   );
 };
