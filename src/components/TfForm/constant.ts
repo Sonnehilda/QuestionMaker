dfc6609dadfee54009ec223e395bd3fc8e97d9ea
__Rals <winhave@naver.com>
2022-05-24 20:20:45 +0900

    Merged Constant Files.

--- src/components/TfForm/constant.ts
+++
@@ -1,9 +0,0 @@
-export const AnswerNotExistException = "Answer field is missing!";
-
-export const TitleNotExistException = "Title field is missing!";
-
-export const SucceededMessage = [
-  "Successfully created ",
-  " question.",
-  'Check your question in the "Saved Questions" tab inside the header!',
-];
---
+++ src/components/share/Form/FormMultipleChoice/constant.ts
@@ -0,0 +1,5 @@
+export const OptionNotExistException = "Option field is missing!";
+
+export const OptionAlreadyExistException = "Option already exists!";
+
+export const OptionNotFulfilledException = "Make at least 2 options!";
