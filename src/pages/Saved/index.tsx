be401946028f853d5f6b648e6476e7af64c55a57
__Rals <winhave@naver.com>
2022-05-26 20:30:39 +0900

    Updated Saved Page.

--- src/pages/Saved/index.tsx
+++ src/pages/Saved/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Banner from "../../components/Banner";
 import List from "../../components/List";
+import McView from "../../components/share/View/ViewMultipleChoice";
 
 const SavedQuestion = () => {
+  const [viewState, setViewState] = useState<string>("");
+
   return (
     <>
+      {viewState && (
+        <McView setViewState={setViewState} questionName={viewState} />
+      )}
       <Banner />
-      <List animation="fade-up" duration="1000" />
+      <List animation="fade-up" duration="1000" setViewState={setViewState} />
     </>
   );
 };
