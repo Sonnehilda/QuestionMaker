adf4d31a1d37f01c25b97a8f09076d202a6efc60
__Rals <winhave@naver.com>
2022-05-27 14:34:16 +0900

    Added Short Answer Question View Component.

---
+++ src/components/share/View/ViewShortAnswer/index.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import Input from "../../../Input";
+
+const Background = styled.div`
+  position: fixed;
+
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  padding-top: 3vh;
+  padding-bottom: 3vh;
+
+  background-color: #fff;
+
+  width: 90vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  border: 0.1vh solid #000;
+  border-radius: 0.5vh;
+  z-index: 2;
+`;
+
+const Leave = styled.div`
+  margin-right: 70.5vh;
+
+  color: #666;
+  font-size: 1.5vh;
+  text-decoration: none;
+
+  cursor: pointer;
+  transition: filter 0.25s;
+
+  :link {
+    color: #666;
+  }
+  :visited {
+    color: #666;
+  }
+
+  :hover {
+    filter: brightness(150%);
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  margin-bottom: 1.5vh;
+  padding-bottom: 0.5vh;
+
+  width: 76vh;
+
+  font-size: 3vh;
+  font-weight: 600;
+  text-align: center;
+
+  border-bottom: 0.1vh solid #aaa;
+`;
+
+interface SaViewProps {
+  questionName: string;
+  setViewState: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SaView = ({ questionName, setViewState }: SaViewProps) => {
+  const [status, setStatus] = useState<boolean>(false);
+
+  const question: string[] = JSON.parse(
+    localStorage.getItem(questionName) || ""
+  );
+
+  useEffect(() => {
+    const close = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setViewState("");
+      }
+    };
+
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+  }, []);
+
+  return (
+    <Background>
+      <Leave onClick={() => setViewState("")}>← Close</Leave>
+      <Wrapper>
+        <Title>{question[0]}</Title>
+      </Wrapper>
+      <Wrapper>
+        <Input word={question[1]} status={status} setStatus={setStatus} />
+      </Wrapper>
+    </Background>
+  );
+};
+
+export default SaView;
--- src/pages/Saved/index.tsx
+++ src/pages/Saved/index.tsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 import Banner from "../../components/Banner";
 import List from "../../components/List";
 import McView from "../../components/share/View/ViewMultipleChoice";
+import SaView from "../../components/share/View/ViewShortAnswer";
 
 const SavedQuestion = () => {
   const [viewState, setViewState] = useState<string>("");
 
   return (
     <>
-      {viewState && (
+      {viewState.includes("MC") && (
         <McView setViewState={setViewState} questionName={viewState} />
+      )}
+      {viewState.includes("SA") && (
+        <SaView setViewState={setViewState} questionName={viewState} />
       )}
       <Banner />
       <List animation="fade-up" duration="1000" setViewState={setViewState} />
