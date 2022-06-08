1dda1b46f57b37193481889ed52864b437a814e2
__Rals <winhave@naver.com>
2022-06-08 19:48:34 +0900

    Added Import & Export Images.

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,3 +3,6 @@ export { default as mc } from "./mc.png";
 export { default as sa } from "./sa.png";
 export { default as tf } from "./tf.png";
 export { default as fc } from "./fc.png";
+
+export { default as importImage } from "./importImage.png";
+export { default as exportImage } from "./exportImage.png";
