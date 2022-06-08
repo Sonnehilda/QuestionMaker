10f4ecb8a16d936a8fd670a3922be069e64c44ba
__Rals <winhave@naver.com>
2022-06-08 19:51:02 +0900

    Updated Make Page.

--- src/pages/Make/index.tsx
+++ src/pages/Make/index.tsx
@@ -1,5 +1,5 @@
 import Banner from "../../components/Banner";
-import Menu from "../../components/Menu";
+import QuestionMenu from "../../components/QuestionMenu";
 import Phrase from "../../components/Phrase";
 import {
   FlashcardMainPhrase,
@@ -11,12 +11,37 @@ import {
   TrueOrFalseQuestionMainPhrase,
   TrueOrFalseQuestionSubPhrase,
 } from "./constant";
+import ActionMenu from "../../components/ActionMenu";
+import ImportModal from "../../components/share/Modal/ImportModal";
+import ExportModal from "../../components/share/Modal/ExportModal";
+import { useEffect, useState } from "react";
 
 const Make = () => {
+  const [viewState, setViewState] = useState<string>("");
+
+  useEffect(() => {
+    const close = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setViewState("");
+      }
+    };
+
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
+      {viewState === "Import" && <ImportModal setViewState={setViewState} />}
+      {viewState === "Export" && <ExportModal setViewState={setViewState} />}
       <Banner />
-      <Menu animation="fade-up" duration="1000" />
+      <QuestionMenu animation="fade-up" duration="1000" />
+      <ActionMenu
+        animation="fade-up"
+        duration="1500"
+        setViewState={setViewState}
+      />
 
       <Phrase
         mainphrase={MultipleChoiceQuestionMainPhrase}
